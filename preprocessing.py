7f76092982402d2bba880087512bc351b91a1fe2
kerins18 <kerins18@gmail.com>
2021-01-28 21:58:06 +0100

    adding calculate returns to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -2,10 +2,8 @@
 Preprocessing Function Librairy
 """
 
-
-
 import pandas as pd
-
+import numpy as np
 
 
 def to_date(df, date_column):
@@ -33,4 +31,40 @@ def proper_col(df):
         proper_col = proper_name(col)
         proper_df[proper_col] = df[col]
     return proper_df
+
+def caculate_real_returns(df):
+    """
+       Calculate Real Returns for 5, 10 & 20 Trading Days
+       INPUT: Assumes the headers have been changed. Example: '<CLOSE>' is 'close'
+    """
+    returns_df = df.copy()
+    
+    ## 5 Trading Day Real Returns
+    returns_df['5TD_return'] = (returns_df['close'] - returns_df['close'].shift(5))/returns_df['close']
+
+    ## 10 Trading Day Real Returns
+    returns_df['10TD_return'] = (returns_df['close'] - returns_df['close'].shift(10))/returns_df['close']
+
+    ## 20 Trading Day Real & Log Returns
+    returns_df['20TD_return'] = (returns_df['close'] - returns_df['close'].shift(20))/returns_df['close']
+    
+    return returns_df
+
+def caculate_log_returns(df):
+    """
+       Calculate Log Returns for 5, 10 & 20 Trading Days
+       INPUT: Assumes the headers have been changed. Example: '<CLOSE>' is 'close'
+    """
+    returns_df = df.copy()
+    
+    ## 5 Trading Day Log Returns
+    returns_df['5TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(5))
+
+    ## 10 Trading Day Log Returns
+    returns_df['10TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(10))
+
+    ## 20 Trading Day Log Returns
+    returns_df['20TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(20))
+    
+    return returns_df
 
