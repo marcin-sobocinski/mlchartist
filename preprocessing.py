7647e900cbf084b6bb80fbbcf0cea85467c2e50b
Charles Montama <c.montama.l@gmail.com>
2021-01-28 19:46:53 +0000

    updating preprocessing notebook and packaging its function into preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,36 @@
+"""
+Preprocessing Function Librairy
+"""
+
+
+
+import pandas as pd
+
+
+
+def to_date(df, date_column):
+    """
+    Convert int of dated_col to to datetime object
+    """
+    dated_col = pd.to_datetime(df[date_column], format=(%Y%m%d))
+    return dated_col
+
+def proper_name(col):
+    """
+    Mapping function for proper_col()
+    """
+    col = col.replace('>', '')
+    col = col.replace('<', '')
+    col = col.lower()
+    return col
+
+def proper_col(df):
+    """
+    Clean column '<TITLE>' to 'title'
+    """
+    proper_df = pd.DataFrame()
+    for col in list(df):
+        proper_col = proper_name(col)
+        proper_df[proper_col] = df[col]
+    return proper_df
+
