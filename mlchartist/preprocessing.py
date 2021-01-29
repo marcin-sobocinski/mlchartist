7a42131ee3108e31dfaca985015c52cd79ac3ff1
Marcin Sobocinski <kensei@hot.pl>
2021-01-29 08:20:19 +0100

    fixed rate of returns calculations

--- mlchartist/preprocessing.py
+++ mlchartist/preprocessing.py
@@ -44,30 +44,30 @@ def proper_col(df):
     return proper_df
 
 
-def caculate_real_returns(df):
+def calculate_real_returns(df):
     """
        Calculate Real Returns for 5, 10 & 20 Trading Days
        INPUT: Assumes the headers have been changed. Example: '<CLOSE>' is 'close'
     """
     returns_df = df.copy()
     ## 5 Trading Day Real Returns
-    returns_df['5TD_return'] = (returns_df['close'] - returns_df['close'].shift(5))/returns_df['close']
+    returns_df['5TD_return'] = (df['close'].shift(-5)/df['close'])-1
     ## 10 Trading Day Real Returns
-    returns_df['10TD_return'] = (returns_df['close'] - returns_df['close'].shift(10))/returns_df['close']
-    ## 20 Trading Day Real & Log Returns
-    returns_df['20TD_return'] = (returns_df['close'] - returns_df['close'].shift(20))/returns_df['close']
+    returns_df['10TD_return'] = (df['close'].shift(-10)/df['close'])-1
+    ## 20 Trading Day Real
+    returns_df['20TD_return'] = (df['close'].shift(-20)/df['close'])-1
     return returns_df
 
-def caculate_log_returns(df):
+def calculate_log_returns(df):
     """
        Calculate Log Returns for 5, 10 & 20 Trading Days
        INPUT: Assumes the headers have been changed. Example: '<CLOSE>' is 'close'
     """
     returns_df = df.copy()
     ## 5 Trading Day Log Returns
-    returns_df['5TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(5))
+    returns_df['5TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(-5))
     ## 10 Trading Day Log Returns
-    returns_df['10TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(10))
+    returns_df['10TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(-10))
     ## 20 Trading Day Log Returns
-    returns_df['20TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(20))
+    returns_df['20TD__log_return'] = np.log(returns_df['close']) - np.log(returns_df['close'].shift(-20))
     return returns_df
