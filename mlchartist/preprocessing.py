b3efdd5468ed5084cd8542c4511f6091470388eb
kerins18 <kerins18@gmail.com>
2021-01-30 16:43:08 +0100

    update windowing function

--- mlchartist/preprocessing.py
+++ mlchartist/preprocessing.py
@@ -105,7 +105,7 @@ def train_test_split(df, test_set_size):
     INPUT: the dataframe to be split, and size of the test set in months or years ('3M' or '2Y')
     OUTPUT: returns a train_set and test_set dataframe, index is set to the date
     
-    EXAMPLE: train_set, test_set = train_test_split(input_df, '3Y')
+    EXAMPLE: train_set, test_set = train_test_split(input_df, '3Y')  --> puts last 3 years in test_set
     """
     if not np.issubdtype(df['date'].dtype, np.datetime64):
         df['date'] = pd.to_datetime(df['date'], format=('%Y-%m-%d'))
@@ -127,6 +127,33 @@ def returns_classification(return_column, returns_threshold):
     return (return_column > returns_threshold).astype(np.int)
 
 
+
+## Ian's window function
+def window_dataframe(df, window=30, stride_size=5, target=['5TD_return'], feature_cols=['RSI', 'Stochastic', 'Stochastic_signal', 
+        'ADI', 'OBV', 'ATR', 'ADX', 'ADX_pos', 'ADX_neg', 'MACD', 'MACD_diff', 'MACD_signal']):
+    """
+    Turns the input dataframe into an array of windowed arrays
+    INPUT: the input dataframe, window size, stride size, target column, feature columns
+    OUTPUT: array of windowed arrays 
+    
+    EXAMPLE: windowed_array = window_dataframe(train_set)
+    """
+    if not np.issubdtype(df['date'].dtype, np.datetime64):
+        df['date'] = pd.to_datetime(df['date'], format=('%Y-%m-%d'))
+    inverse_df = df.sort_values(by="date", ascending=False)
+    feature_array = []
+    target_array = []
+    for column in inverse_df:
+        if column in feature_cols: 
+            feature_array.append(window_column(inverse_df[column], window, stride_size))
+            
+        elif column in target:
+            target_array.append(window_column(inverse_df[column], window, stride_size))
+            
+    
+    return np.array(feature_array), np.array(target_array)
+
+
 ## this function is called in window_dataframe function
 def window_column(df_series, window_size=30, stride_size=5):
     """
@@ -141,26 +168,4 @@ def window_column(df_series, window_size=30, stride_size=5):
     n = np_array.strides[0]
     return np.lib.stride_tricks.as_strided(
         np_array, shape=(nrows, window_size), strides=(stride_size*n, n))
-
-
-def window_dataframe(df, window=30, stride_size=5):
-    """
-    Turns the input dataframe into an array of windowed arrays
-    INPUT: the input dataframe, window size, stride size
-    OUTPUT: array of windowed arrays 
-    
-    EXAMPLE: windowed_array = window_dataframe(train_set)
-    """
-    if not np.issubdtype(df['date'].dtype, np.datetime64):
-        df['date'] = pd.to_datetime(df['date'], format=('%Y-%m-%d'))
-    inverse_df = df.sort_values(by="date", ascending=False)
-    output_array = []
-    for column in inverse_df:
-        if column not in []: ## exclude columns from being windowed by adding the column name
-            output_array.append(window_column(inverse_df[column], window, stride_size))
-    
-    return output_array
-
-
-
 
