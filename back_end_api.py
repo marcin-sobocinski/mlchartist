f50a8c46bed8aee2d1a9eb6b9b7cb58a7d453ca6
Charles Montama <c.montama.l@gmail.com>
2021-02-16 21:54:14 +0000

    start to implement the UI and back-end to prepare the web interface

---
+++ back_end_api.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+import joblib
+import pandas as pd
+import pytz
+from flask import Flask
+from flask import request
+from flask_cors import CORS
+from termcolor import colored
+
+from TaxiFareModel.gcp import download_model
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/')
+def index():
+    return 'OK'
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
